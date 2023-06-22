cf7e9854426e9464403fc13ccac4d35c8d38b601
SanzidaH <sanzidahoquekona@gmail.com>
2023-06-22 05:19:09 -0500

    Added scripts

---
+++ script/raw-a.py
@@ -0,0 +1,165 @@
+from __future__ import division 
+import csv 
+import sys 
+from collections import defaultdict 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import glob
+
+def main(filepath=None, traceID=None, maxSuccess=None):
+    count = []
+    node_count = 0
+    latency_sum = []
+    tp = []
+    sent = []
+    nodes = {}
+    for i in range(0,5000): #initialization of the lists
+        tp.append(0)
+        count.append(0)
+        latency_sum.append(0)
+        sent.append(0)
+    if filepath is None and maxSuccess is None:
+        sys.stderr.write("usage: python3 %s <file-path> <expected-success-number>\n")
+        return 1
+    xmax = 250 # node no
+    m = 50
+    cumulatedFullLatencies = []  #lantencies for all files
+    filesToProcess = glob.glob(filepath + "*" ) #search for files that's used to search for files that match specific file pattern or name
+    print(filesToProcess)
+    f2 = open(traceID + "-Average_Delay-.txt","w")
+    f3 = open(traceID + "-Success_Ratio-.txt","w")
+    for filename in filesToProcess:
+        pending = {}
+        pending1 = {}
+        senti = {}
+        fullLatencies = []
+        totalSent = 0
+        latencies = defaultdict(lambda: [])
+        print("Trying :" + filename)
+        with open(filename, 'r') as csvh:
+            #dialect = csv.Sniffer().sniff(csvh.read(10*1024))
+            dialect = csv.Sniffer().has_header(csvh.read())
+            csvh.seek(0)
+            reader = csv.DictReader(csvh, dialect=dialect)
+
+            for row in reader:     # nodeid, name, event, time
+                node = int(row['nodeid'])  #current node
+                if node not in nodes:
+                        nodes[node] = 1 #storing all the nodes in nodes[]
+                        node_count += 1 # counting all nodes
+                time = float(row['time'])  #current time for the event
+                if row['event'] == 'sent': # current event = sent
+                  # totalSent = totalSent + 1 # counting total sent
+                   senti[(row['name'])]=0 
+                   if (row['name']) not in pending and int(time) <=(m-20) and int(time) > 5:
+                      totalSent = totalSent + 1 # counting total sent
+                      pending[(row['name'])] = time # storing unique sent packet as pending[name][time] which is not in pending
+                  # else:
+                   #   print(str(row['name'])+ " same interest ...") 
+                   sent[int(time)] += 1 # storing number sent packet in particular time sent[time][sent_count]
+                   pending1[ row['name']] = time # storing all the sent packet as pending[name][time] like PIT
+                elif (row['event'] == 'received'): # current event = received
+                    # and (row['(hopCount)/seq'] == '6'):
+                    #assert((node, row['name']) in pending)
+                    if (row['name']) in pending:
+                      latencies[node].append(1. * (time - pending[(row['name'])])) # node wise latency
+                      latency_sum[int(time)] += 1000. * (time - pending[( row['name'])]) # total latency for all node
+                      count[int(time)] += 1 # counting total received
+                      tp[int(pending1[( row['name'])])] += 1 # satisfied interest track and count
+                      del pending[( row['name'])] # remove the satisfied interest from pending[][]
+                      senti[(row['name'])]=1
+                    else: 
+                      print( str( row['name'])  + " is recived, but not sent")
+                   # del pending[( row['name'])] # remove the satisfied interest from pending[][]
+                    #del pending1[(node, row['name'])]
+                #else:
+                    #del pending[(node, row['name'])]
+                    #del pending1[(node, row['name'])]
+            ###### All row reading is done
+            print("********* not received *** ")
+            cnt_p = 0
+            for k,v in senti.items():
+               # print("*********")
+                if(v==0):
+                    cnt_p = cnt_p + 1
+                    print(k,v)
+                    
+       #     print("total received: " + str(cnt_p))
+       #     print("total sent: " + str(totalSent))
+       #     print("total success: " + str(cnt))
+            c = 0
+            f = open(traceID + "UnsortedLatencies.txt","w")
+            cnt = 0
+            total = 0
+            for key in latencies: # for each node == key
+                for latencyVal in latencies[key]: # for each latency in each node
+                    fullLatencies.append(latencyVal) # unsorted list for all success latency
+                    f.write(str(latencyVal) + "\n") # wrtitng unsorted latency
+                    total = total + float(latencyVal) # sum of all latency
+                    cnt = cnt + 1 # count of success
+            totalsucceed = total
+            print("total  not received: " + str(cnt_p))
+            print("total sent: " + str(totalSent))
+            print("total success: " + str(cnt))
+
+            print("total delay of successful sent: " + str(totalsucceed))
+            print("Accurate avg delay " + str(float(totalsucceed/cnt)))
+            array = filename.split("-")
+           # for word in array:
+           #     print("word " +str(word))
+            freq =  str(array[3])
+            freq = freq[:-4]
+            print("freq " + freq)
+            f2.write(str(array[2]) + "  "  + freq + "  " + str(totalsucceed/cnt) + "\n")
+            total = total + ((totalSent-cnt)*100) # setting high latency for unsuccesful sent
+           # f = open(traceID + "avgRes.txt","w")
+           # f.write("3" + " "  + str(float(total/totalSent)) + "\n")
+           # f.write("3" + " "  + str(float(total/totalSent) + "\n")
+            print("count for succesful task:" + str(cnt))
+            print("totalSent: " + str(totalSent))
+            print("total response time after setting hgh delay or unsuccess: " + str(total) )
+            print("response time avg: " + str(float(total/totalSent)))
+            print(str(filename) + "Total Successful interest : " + str(len(fullLatencies)))
+            print(str(filename) + "Success Rate:(success sent/ total sent): " + str( cnt /totalSent))
+            f3.write(str(array[2]) + "  "  + freq + "  " + str(cnt/totalSent) + "\n")
+            maxSuccess = len(fullLatencies)
+            #maxSuccess = 1
+            #if len(fullLatencies) < int(maxSuccess):
+            for x in range(int(maxSuccess) - len(fullLatencies)):
+                fullLatencies.append(xmax)
+            cumulatedFullLatencies.extend(fullLatencies)
+    print(node_count) 
+    f2.close()
+    f3.close()
+    for i in range(0,180):
+        if(count[i]==0):
+                latency_sum[i] = 0
+        else:
+                latency_sum[i] = latency_sum[i]/count[i] ## Average latency
+
+    for i in range(0,180):
+        if(count[i]==0):
+                latency_sum[i] = 0
+        else:
+                latency_sum[i] = latency_sum[i]/count[i]
+        #print("Avg response time: " + str(latency_sum[i]) )
+
+ 
+    for i in range(0,180):
+        if(sent[i]==0):
+                tp[i] = 1
+        else:
+                tp[i] = tp[i]/sent[i]
+   
+ 
+    #Dump Latencies
+    cumulatedFullLatencies.sort()
+    f = open(traceID+"SortedLatencies.txt","w")
+    for each in cumulatedFullLatencies:
+        f.write(str(each) + "\n")
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main(*sys.argv[1:]))
---
+++ script/raw_b3.py
@@ -0,0 +1,197 @@
+from __future__ import division
+import csv
+import sys
+from collections import defaultdict
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+#global first
+#first = 3
+def main(filepath=None, traceID=None, maxSuccess=None):
+    count = []
+    node_count = 0
+    latency_sum = []
+    tp = []
+    sent = []
+    nodes = {}
+    global first
+    first = 3
+    # first = [];
+   # barWidth = 0.25
+   # fig = plt.subplots(figsize =(12, 8))
+    for i in range(0, 5000):  # initialization of the lists
+        tp.append(0)
+        count.append(0)
+        latency_sum.append(0)
+        sent.append(0)
+    if filepath is None and maxSuccess is None:
+        sys.stderr.write("usage: python3 %s <file-path> <expected-success-number>\n")
+        return 1
+    filesToProcess = glob.glob(
+        filepath + "*"
+    )  # search for files that's used to search for files that match specific file pattern or name
+    print(filesToProcess)
+    # f2 = open(traceID + "-Average_Delay-.txt","w")
+    # f3 = open(traceID + "-Success_Ratio-.txt","w")
+   # global first
+    for filename in filesToProcess:
+        array = filename.split("-")
+        # f3 = open(traceID + "-" + array[2] + "-" + str(array[3])[:-4] + "-Server.txt","w")
+        dicto = dict.fromkeys(range(100), 0)
+        dictp = dict.fromkeys(range(100), 0)
+        # dicto = dict(zip(range(10), repeat(0)))
+        # dictp = dict(zip(range(10), repeat(0)))
+        print("Trying :" + filename)
+        with open(filename, "r") as csvh:
+            # dialect = csv.Sniffer().sniff(csvh.read(10*1024))
+           # dialect = csv.Sniffer().has_header(csvh.read(1024))
+            dialect = csv.Sniffer().sniff(csvh.readline())
+            csvh.seek(0)
+            reader = csv.DictReader(csvh, dialect=dialect)
+            for row in reader:  # nodeid, name, servercount,status
+               # print(row["status"])
+               # print(row["nodeid"])
+                if str(row["status"]) == "OVERLOADED":
+                    dicto[int(row["nodeid"])] = int(dicto[int(row["nodeid"])]) + int(1)
+                elif str(row["status"]) == "Data Processing":
+                    dictp[int(row["nodeid"])] = int(dictp[int(row["nodeid"])]) + int(1)
+            list1 = []
+            list2 = []
+            for (k, v), (k2, v2) in zip(dicto.items(), dictp.items()):
+                if str(v) == "0" and str(v2) == "0":
+                    list1.append(k)
+                    list2.append(k2)
+            for i in list1:
+                del dicto[i]
+            for i in list1:
+                del dictp[i]
+
+            freq = str(array[2])
+            sorted_dict1 = dict(sorted(dicto.items()))
+            sorted_dict2 = dict(sorted(dictp.items()))
+            
+                
+           # for k, v in sorted_dict1.items():
+            #    print(k, v)
+           # for k, v in sorted_dict2.items():
+            #    print(k, v)
+           # print(array[3])
+            if(str(array[3]) == "0"):
+                print(array[3])
+                print(first)
+                global x1
+                x1 = 1
+                X1 = []
+                X2 = []
+                Y1 = []
+                Y2 = []
+                X1 = list(sorted_dict1.keys())
+                Y1 = list(sorted_dict1.values())
+                X2 = list(sorted_dict2.keys())
+                Y2 = list(sorted_dict2.values())
+                print(X1)
+                print(Y1)
+                print(Y2)
+
+               # print(globals())
+                if('y3' in globals() or first == 1):
+                    X_axis = np.arange(len(X1))
+                    width = 0.4
+                    # First set of bars (ComVes)
+                    plt.bar(X_axis - width/2, Y1, width, label='ComVes(Overloaded)', color="g", edgecolor='black')
+                    plt.bar(X_axis - width/2, Y2, width, label='ComVes(Processed)', color="y", bottom=Y1, edgecolor='black', hatch='/' )
+                    # Second set of bars (Proposed Strategy)
+                    plt.bar(X_axis + width/2, Y3, width, label='Proposed(Overloaded)', color="m", edgecolor='black')
+                    plt.bar(X_axis + width/2, Y4, width, label='Proposed(Processed)', color="c", bottom=Y3 , edgecolor='black', hatch='.')
+                    plt.xticks(X_axis, X1)
+                    plt.xlabel("Server")
+                    plt.ylabel("Number Of Task")
+                    plt.title("Number of Task per Server")
+                    plt.legend()
+                    arr = filename.split("-")
+                    plt.savefig(traceID + "-fig1-" + arr[2] + "-" + arr[3] + "-Server-d1.png")
+                    list1.clear()
+                    list2.clear()
+                    dicto.clear()
+                    dictp.clear()
+                    sorted_dict1.clear()
+                    first = 0
+                    print("ComVes(Overloaded) ")
+                    print(Y1)
+                    print("ComVes(Processed)")
+                    print(Y2)
+                    print("Proposed(Overloaded)")
+                    print(Y3)
+                    print("Proposed(Processed)")
+                    print(Y4)
+
+    #                global first
+                    
+                    print(first)
+              # plt.bar(X1, Y1, color="y")
+               # plt.bar(X1, Y2, bottom=Y1, color="g")
+#                plt.bar(X2, Y2, color="y")
+#                plt.bar(X2, Y3, bottom=Y1, color="g")
+              #  plt.xlabel("Server")
+              #  plt.ylabel("Number Of Task")
+              #  plt.legend(["OVERLOADED", "DATA PROCESSED"])
+              #  plt.title("Number of Task per Server")
+                # plt.legend()
+              #  arr = filename.split("-")
+              #  plt.savefig(traceID + "-" + arr[2] + "-" + arr[3] + "-Server-d0.png")
+            if(str(array[3]) =="1"):
+                print(array[3])
+                print(first)
+                X3 = []
+                X4 = []
+                Y3 = []
+                global y3
+                y3 = 1
+                Y4 = []
+                X3 = list(sorted_dict1.keys())
+                Y3 = list(sorted_dict1.values())
+                X4 = list(sorted_dict2.keys())
+                Y4 = list(sorted_dict2.values())
+               # print(X1)
+               # print(Y3)
+               # print(Y4)
+                #print(globals())
+                if('x1' in globals() or first == 0):
+                    X_axis = np.arange(len(X3))
+                    width = 0.4
+                    # First set of bars (ComVes)
+                    plt.bar(X_axis - width/2, Y1, width, label='ComVes(Overloaded)', color="g", edgecolor='black')
+                    plt.bar(X_axis - width/2, Y2, width, label='ComVes(Processed)', color="y", bottom=Y1, edgecolor='black', hatch='/' )
+                    # Second set of bars (Proposed Strategy)
+                    plt.bar(X_axis + width/2, Y3, width, label='Proposed(Overloaded)', color="m", edgecolor='black')
+                    plt.bar(X_axis + width/2, Y4, width, label='Proposed(Processed)', color="c", bottom=Y3 , edgecolor='black', hatch='.')
+                    print("ComVes(Overloaded) ")
+                    print(Y1)
+                    print("ComVes(Processed)")
+                    print(Y2)
+                    print("Proposed(Overloaded)")
+                    print(Y3)
+                    print("Proposed(Processed)")
+                    print(Y4)
+                    plt.xticks(X_axis, X1)                
+                    plt.xlabel("Server")
+                    plt.ylabel("Number Of Task")
+                    plt.title("Number of Task per Server")
+                    plt.legend()
+                    arr = filename.split("-")
+                    plt.savefig(traceID + "-fig1-" + arr[2] + "-" + arr[3] + "-Server-d1.png")
+                    list1.clear()
+                    list2.clear()
+                    dicto.clear()
+                    dictp.clear()
+                    sorted_dict1.clear()
+                    sorted_dict2.clear()
+                    first = 1
+     #               global first
+                   # first = first + 1
+                   # print(first)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main(*sys.argv[1:]))
